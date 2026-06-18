ec70374dfeffeacf1fd6e9af687e1706b6d26421
Nikhil Nath R <nikhil@artusacademy.com>
2026-06-18 12:36:27 +0530

    activvate link

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
   }, []);
 
   // Uncomment the following lines to enable the quiz
-  const QUIZ_ENABLED = false;
+  const QUIZ_ENABLED = true;
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-black font-sans">
